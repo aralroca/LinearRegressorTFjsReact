5a90f21b92f08af3bdd8e832903f9b0c8565407b
Aral Roca Gomez <aral-rg@hotmail.com>
2019-01-27 13:10:37 +0100

    Updates

---
+++ precache-manifest.d80df3f9624913ea61ba1fbb12b80ee1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02f95ea0d6f803b2b2ce",
+    "url": "/LinearRegressorTFjsReact/static/js/runtime~main.02f95ea0.js"
+  },
+  {
+    "revision": "01d5206296fa6803f072",
+    "url": "/LinearRegressorTFjsReact/static/js/main.01d52062.chunk.js"
+  },
+  {
+    "revision": "63c18a8af12145be226b",
+    "url": "/LinearRegressorTFjsReact/static/js/1.63c18a8a.chunk.js"
+  },
+  {
+    "revision": "01d5206296fa6803f072",
+    "url": "/LinearRegressorTFjsReact/static/css/main.a1a7ee33.chunk.css"
+  },
+  {
+    "revision": "e9d47093f3d875d37489962440943870",
+    "url": "/LinearRegressorTFjsReact/index.html"
+  }
+];
